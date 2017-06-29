bad14ee62b578dfe7a71b6ac04b96c8f0a8d80a7
Akhil Lawrence <akhilputhiry@gmail.com>
2017-06-29 13:09:19 +0530

    updated translator

--- jarvis/static/js/translator.js
+++ jarvis/static/js/translator.js
@@ -19,4 +19,14 @@ function process(results){
   // console.log(results);
   transcript = $.trim(results[results.length - 1][0].transcript);
   $("#translated-text").html(transcript);
+
+  // get translated text
+  $.post('/apis/translate', {'text': transcript, 'format': 'text'}).done(function(){
+
+  });
+
+  // get translated speech
+  $.post('/apis/translate', {'text': transcript, 'format': 'audio'}).done(function(){
+
+  });
 }
