e1e8ca0c4641f4669b89bb690e7fc88e2e9a650f
Akhil Lawrence <akhilputhiry@gmail.com>
2017-06-29 09:08:56 +0530

    updated home page

--- jarvis/blueprint.py
+++ jarvis/blueprint.py
@@ -5,7 +5,8 @@ jarvis = Blueprint(
     'jarvis',
     __name__,
     static_folder='static',
-    template_folder='templates'
+    template_folder='templates',
+    static_url_path='/static/jarvis'
 )
 
 
